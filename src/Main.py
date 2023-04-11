92f0b569c6821a34f269504caaf795b7a0f275b5
kelvinra <13521005@mahasiswa.itb.ac.id>
2023-04-11 16:49:16 +0700

    fix switch page

--- src/Main.py
+++ src/Main.py
@@ -2,32 +2,30 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-
-class MainWindow(QDialog):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        loadUi('src/BuilderUI/Main.ui', self)
-        self.Logo.clicked.connect(self.openMenu)
-
-    def openMenu(self):
-        widget.setCurrentIndex(1)
+from PyQt5.QtCore import *
+import MainWindow as mw
         
 
 
-class MenuWindow(QDialog):
-    def __init__(self):
-        super(MenuWindow, self).__init__()
-        loadUi('src\BuilderUI\Menu.ui', self)
 
 
-app = QApplication(sys.argv)
-window = MainWindow()
-widget = QtWidgets.QStackedWidget()
-menu = MenuWindow()
-widget.addWidget(window)
-widget.addWidget(menu)
-widget.setFixedWidth(1920)
-widget.setFixedHeight(1024)
-widget.show()
+class app(QApplication):
+    def __init__(self, argv):
+        super(app, self).__init__(argv)
+        self.window = mw.MainWindow()
+        # self.menu = MenuWindow()
+        self.widget = QtWidgets.QStackedWidget()
+        self.widget.addWidget(self.window)
+        self.widget.addWidget(self.menu)
+        self.widget.setFixedWidth(1920)
+        self.widget.setFixedHeight(1024)
+        self.widget.show()
+        self.window.Logo.clicked.connect(lambda: self.widget.setCurrentIndex(1))
+    
 
-sys.exit(app.exec_())
+        
+
+
+run = app(sys.argv)
+
+sys.exit(run.exec_())
---
+++ src/MainWindow.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi('src/BuilderUI/Main.ui', self)
+
+    
