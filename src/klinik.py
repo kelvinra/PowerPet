0434c560695811754f745f3a327d35a6f3c0457e
kartinicops <13521026@std.stei.itb.ac.id>
2023-04-16 17:30:43 +0700

    fix data klinik

--- src/klinik.py
+++ src/klinik.py
@@ -14,8 +14,8 @@ class klinikWindow(QDialog):
         self.satubtn = self.findChild(QPushButton, 'satu')
         self.tableWidget.setColumnWidth(0, 500)
         self.tableWidget.setColumnWidth(1, 600)
-        self.tableWidget.setColumnWidth(2, 300)
-        self.tableWidget.setColumnWidth(3, 400)
+        self.tableWidget.setColumnWidth(2, 250)
+        self.tableWidget.setColumnWidth(3, 200)
 
         self.load_data()
 
@@ -25,14 +25,13 @@ class klinikWindow(QDialog):
         cur.execute("SELECT * FROM Klinik")
         data = cur.fetchall()
 
-        self.tableWidget.setColumnCount(len(data[0])-1) # Kurangi 1 karena IDKlinik dihapus
+        # set jumlah kolom dan baris tableWidget
+        self.tableWidget.setColumnCount(len(data[0])-1) # kurangi 1 untuk menghilangkan kolom IDKlinik
         self.tableWidget.setRowCount(len(data))
-        self.tableWidget.setHorizontalHeaderLabels(['ID Klinik', 'Nama Klinik', 'Alamat', 'Jam Praktek', 'Telepon'])
+        self.tableWidget.setHorizontalHeaderLabels(['Nama Klinik', 'Alamat', 'Jam Praktek', 'Telepon'])
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
 
-        self.tableWidget.takeHorizontalHeaderItem(0) # Hapus judul header kolom IDKlinik
-
+        # tampilkan data ke dalam tableWidget
         for row_num, row_data in enumerate(data):
-            for col_num, col_data in enumerate(row_data[1:]): # Mulai dari indeks ke-1 karena IDKlinik dihapus
+            for col_num, col_data in enumerate(row_data[1:]): # mulai dari indeks ke-1 untuk menghilangkan kolom IDKlinik
                 self.tableWidget.setItem(row_num, col_num, QtWidgets.QTableWidgetItem(str(col_data)))
-
-        self.tableWidget.setHorizontalHeaderLabels(['Nama Klinik', 'Alamat', 'Jam Praktek', 'Telepon']) # Atur ulang judul header kolom
