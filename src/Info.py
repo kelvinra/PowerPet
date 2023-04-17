41e3d19feb8e4320077eedf6d04e626338f9d090
varrazha <varrazhazzandraabrar11mia7@gmail.com>
2023-04-17 12:49:22 +0700

    upd stackwidg

--- src/Info.py
+++ src/Info.py
@@ -7,38 +7,14 @@ class InfoWindow(QDialog):
     def __init__(self):
         super(InfoWindow, self).__init__()
         loadUi('src/BuilderUI/Info.ui', self)
+        self.indtbtn= self.findChild(QPushButton, 'indt')
+        self.mafabtn= self.findChild(QPushButton, 'mafa')
+        self.cakebtn= self.findChild(QPushButton, 'cake')
 
-    # def showInfo() :
-    #     cursor = Binatang.conn.cursor()
-    #     cursor.execute("SELECT * FROM Hewan")
-    #     data = cursor.fetchall()
-    #     return data
-
-
-
-    # def infoInput(idHewan):
-    #     cursor = Binatang.conn.cursor()
-    #     cursor.execute("SELECT * FROM Hewan WHERE id = ? ", (idHewan,))
-    #     data = cursor.fetchone()
-    #     if data:
-    #         print(data)
-    #         return data
-    #     else:
-    #         print("Hewan dengan ID tersebut tidak ditemukan.")
-    #         return None
-
-    # def inputInfo () :
-    #     # minta pengguna memasukkan nilai argumen
-    #     id = input("Masukkan ID hewan: ")
-    #     nama = input("Masukkan nama hewan: ")
-    #     jenis = input("Masukkan jenis hewan: ")
-    #     makanan = input("Masukkan makanan hewan: ")
-    #     umur = input("Masukkan umur hewan: ")
-    #     birthdate = input("Masukkan tanggal lahir hewan: ")
-    #     gender = input("Masukkan jenis kelamin hewan: ")
-    #     berat = input("Masukkan berat hewan: ")
-    #     hewan = Hewan(id, nama, jenis, makanan, umur, birthdate, gender, berat)
-    #     print (hewan)
+        self.indtbtn.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+        self.mafabtn.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.cakebtn.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+    
 
 
 
